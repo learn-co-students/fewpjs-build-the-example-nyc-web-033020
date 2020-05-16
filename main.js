4d57502113fc942dd4764314d39a8e9a4bce4136
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-05-16 14:21:34 -0700

    Done.

--- main.js
+++ main.js
@@ -4,10 +4,38 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const modalDiv = document.getElementById('modal');
+const modalMessagePar = document.getElementById('modal-message')
 
+modalDiv.classList.add('hidden');
 
+document.body.addEventListener('click', (event) => {
+  if (event.target.className === 'like-glyph' || event.target.className === 'like-glyph activated-heart'){
+    mimicServerCall()
+    .then(resp => {
+      if (event.target.innerHTML === EMPTY_HEART){
+        event.target.innerHTML = FULL_HEART
+        event.target.classList.add('activated-heart')
+      } else {
+        event.target.innerHTML = EMPTY_HEART
+        event.target.classList.remove('activated-heart')
+      }
+    })
+    .catch(error => {
+      // console.log(error)
+      modalMessagePar.innerText = error;
+      modalDiv.classList.remove('hidden');
 
-//------------------------------------------------------------------------------
+      setTimeout(function(){
+        modalDiv.classList.add('hidden');
+      }, 5000);      
+    });
+
+  }
+
+});
+    
+//-);-----------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
 
